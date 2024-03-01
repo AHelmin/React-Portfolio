fddc0fb0e97a10c963bd594f4baa5853fb270e07
helm0127 <helm0127@morris.umn.edu>
2024-02-29 21:28:29 -0600

    started to draft pages

--- src/pages/About-me.jsx
+++ src/pages/About-me.jsx
@@ -7,7 +7,25 @@ export default function AboutMe() {
   return (
     <>
       <h2 className="text-left">About Me</h2>
+      <p>
+        I worked as a chemist and got my bachelor's degree from the
+        University of Minnesota. After spending five years in the field,
+        I felt the need for a change and decided to pursue an exciting
+        career in coding.
+      </p>
 
+      <p>
+        Right now, I'm fully immersed in a challenging bootcamp program.
+        It's intense, but I'm confident that with hard work, I'll
+        successfully finish the program. I have the determination to see
+        it through, and my goal is to become a full stack developer with
+        the skills I'm gaining from this bootcamp.
+      </p>
+      <p>
+        My coursework includes HTML, CSS, JavaScript, Node.js, React, APIs.
+        This list is by no means an exhaustive list of the coursework that I
+        have yet to experience.
+      </p>
 
     </>
   )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,8 +6,52 @@ export default function Portfolio() {
 
   return (
     <>
-      <h2 className="text-left">Portfolio</h2>
+      <body >
+        <div className="d-flex container-fluid flex-wrap-column flex-column mt-5 ms-5">
+          <h2 className="text-left">Portfolio</h2>
+          <div classNameName="d-flex container-fluid flex-wrap-column flex-md-row flex-column">
+            <div className="col-12">
+              <div className="container-fluid row no-gutters justify-content-around">
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
 
+                  </div>
+                </div>
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
+
+                  </div>
+                </div>
+              </div>
+              <div className="container-fluid row no-gutters justify-content-around">
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
+
+                  </div>
+                </div>
+
+
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
+
+                  </div>
+                </div>
+              </div>
+              <div className="container-fluid row no-gutters justify-content-around">
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
+                  </div>
+                </div>
+                <div className="card five-day-card col-6">
+                  <div className="card-body">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </body >
 
     </>
   )
